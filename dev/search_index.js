e604b263aae7355da53e60612e76ac6138ae9ffa
Documenter.jl <documenter@juliadocs.github.io>
2026-03-10 15:26:43 +0000

    build based on 559b7ca

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ColoumbBenchmark","page":"Home","title":"ColoumbBenchmark","text":"Documentation for ColoumbBenchmark.\n\n","category":"section"}]
+}
